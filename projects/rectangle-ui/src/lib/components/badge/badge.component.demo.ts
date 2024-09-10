9c33f39712812989aef66574ff3682c078348c32
Jarrett Huang <jarrett@jhuang.ca>
2024-09-09 22:16:25 -0700

    Remove index.ts files.

--- projects/rectangle-ui/src/lib/components/badge/badge.component.demo.ts
+++ projects/rectangle-ui/src/lib/components/badge/badge.component.demo.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { BadgeComponent } from "@/components/badge";
+import { BadgeComponent } from "@/components/badge/badge.component";
 
 @Component({
   selector: "rectangle-ui-badge-demo",
