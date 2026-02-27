f9ef2f708d60c11dcfd5e470f5fa2549d2eeb3d0
Jarrett Huang <jarrett@jhuang.ca>
2026-02-27 00:46:25 -0800

    Set demo control widths to w-40 for input, combobox, and dropdown.

--- projects/rectangle-ui/src/lib/components/input/input.component.demo.ts
+++ projects/rectangle-ui/src/lib/components/input/input.component.demo.ts
@@ -4,7 +4,7 @@ import { InputComponent } from "@/components/input/input.component";
 @Component({
   selector: "rui-input-demo",
   template: `
-    <div class="w-80">
+    <div class="w-40">
       <rui-input placeholder="Email" type="email"></rui-input>
     </div>
   `,
