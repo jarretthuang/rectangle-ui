764f14924dc80a1576f426220f0fc0f234db1b05
Jarrett Huang <jarrett@jhuang.ca>
2026-02-28 14:28:57 -0800

    Restrict public API to prod components and enforce it with a test.

---
+++ projects/rectangle-ui/src/public-api.spec.ts
@@ -0,0 +1,19 @@
+import * as publicApi from "./public-api";
+
+describe("public-api exports", () => {
+  it('should export all and only "prod" components', () => {
+    const exportedComponents = Object.keys(publicApi)
+      .filter((name) => name.endsWith("Component"))
+      .sort();
+
+    expect(exportedComponents).toEqual([
+      "BadgeComponent",
+      "ButtonComponent",
+      "ComboboxComponent",
+      "DropdownComponent",
+      "DropdownItemComponent",
+      "IconComponent",
+      "InputComponent",
+    ]);
+  });
+});
