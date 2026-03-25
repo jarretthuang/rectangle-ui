bf0908984a8f839c936a245746172e2a1b05b898
Jarrett Huang <jarrett@jhuang.ca>
2026-03-25 00:11:46 -0700

    Fix the demo SSR bootstrap entrypoint.

--- projects/demo/src/main.server.ts
+++ projects/demo/src/main.server.ts
@@ -1,8 +1,7 @@
-import { BootstrapContext, bootstrapApplication } from "@angular/platform-browser";
+import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { config } from "./app/app.config.server";
 
-const bootstrap = (context: BootstrapContext) =>
-  bootstrapApplication(AppComponent, config, context);
+const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 export default bootstrap;
