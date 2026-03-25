028c9019b9e668af397557a8adcbaebcbbc97ef5
Jarrett Huang <jarrett@jhuang.ca>
2026-03-24 23:52:40 -0700

    Add variant icons to Alert. Update tests.

--- projects/rectangle-ui/src/lib/components/alert/alert.component.spec.ts
+++ projects/rectangle-ui/src/lib/components/alert/alert.component.spec.ts
@@ -14,18 +14,24 @@ describe("AlertComponent", () => {
 
   it("should create", () => expect(component).toBeTruthy());
 
-  it("should render an alert region by default", () => {
+  it("should render an alert region with an icon by default", () => {
     const alertElement: HTMLElement = fixture.nativeElement.querySelector("[role='alert']");
+    const iconElement: HTMLElement = fixture.nativeElement.querySelector("rui-icon");
 
     expect(alertElement).not.toBeNull();
     expect(alertElement.className).toContain("border-primary-300");
+    expect(alertElement.className).toContain("gap-2");
+    expect(iconElement).not.toBeNull();
   });
 
-  it("should apply success classes", () => {
+  it("should apply success classes and icon", () => {
     component.variant = "success";
+    fixture.detectChanges();
 
     const classes = (component as unknown as { classes: string[] }).classes.join(" ");
+    const icon = (component as unknown as { icon: string }).icon;
 
     expect(classes).toContain("emerald");
+    expect(icon).toContain("<svg");
   });
 });
