7dbb5d814c3afa942e1387bb337d9da52d6d2e97
Jarrett Huang <jarrett@jhuang.ca>
2026-03-26 21:44:32 -0700

    Fix the Progress demo host width.

--- projects/rectangle-ui/src/lib/components/progress/progress.component.demo.ts
+++ projects/rectangle-ui/src/lib/components/progress/progress.component.demo.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, HostBinding } from "@angular/core";
 import { ProgressComponent } from "./progress.component";
 
 @Component({
   selector: "rui-progress-demo",
   template: `
-    <div class="w-full max-w-md space-y-2">
+    <div class="space-y-2">
       <rui-progress [value]="25"></rui-progress>
       <rui-progress [value]="66"></rui-progress>
       <rui-progress [value]="90"></rui-progress>
@@ -12,4 +12,6 @@ import { ProgressComponent } from "./progress.component";
   `,
   imports: [ProgressComponent],
 })
-export class ProgressDemoComponent {}
+export class ProgressDemoComponent {
+  @HostBinding("class") hostClasses = "block w-full max-w-md";
+}
