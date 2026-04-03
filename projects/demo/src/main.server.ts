29ff33a689555c5e9b7ab77f9642cf6adb0c6074
Jarrett Huang <jarrett@jhuang.ca>
2026-04-02 23:36:37 -0700

    [25] Fix demo SSR bootstrap typing (#26)
    * Fix demo SSR bootstrap typing.
    
    * Fix SSR bootstrap for prerender builds

--- projects/demo/src/main.server.ts
+++ projects/demo/src/main.server.ts
@@ -1,8 +1,13 @@
-import { BootstrapContext, bootstrapApplication } from "@angular/platform-browser";
+import { ApplicationRef } from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { config } from "./app/app.config.server";
 
-const bootstrap = (context: BootstrapContext) =>
-  bootstrapApplication(AppComponent, config, context);
+const bootstrap = ((context?: unknown) =>
+  (bootstrapApplication as (...args: unknown[]) => Promise<ApplicationRef>)(
+    AppComponent,
+    config,
+    context
+  )) as () => Promise<ApplicationRef>;
 
 export default bootstrap;
