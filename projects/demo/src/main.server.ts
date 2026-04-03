69484e6f621219db5cea2e074e6235d25c87e899
Jarrett Huang <jarrett@jhuang.ca>
2026-04-02 23:33:31 -0700

    Fix SSR bootstrap for prerender builds

--- projects/demo/src/main.server.ts
+++ projects/demo/src/main.server.ts
@@ -1,7 +1,13 @@
+import { ApplicationRef } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { config } from "./app/app.config.server";
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = ((context?: unknown) =>
+  (bootstrapApplication as (...args: unknown[]) => Promise<ApplicationRef>)(
+    AppComponent,
+    config,
+    context
+  )) as () => Promise<ApplicationRef>;
 
 export default bootstrap;
