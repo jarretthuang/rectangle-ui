6c68f1afa918d8704d2818874e2bd0968309c1a6
Jarrett Huang <jarrett@jhuang.ca>
2026-04-15 10:52:13 -0700

    Release skeleton component (#29)
    * Release skeleton component
    
    * Make skeleton visible in the production demo list.
    
    * Resolve demo pages from the full page dataset.
    
    * Make the skeleton demo more visible.
    
    * Center the skeleton preview in the demo container.

--- projects/rectangle-ui/src/lib/components/skeleton/skeleton.component.demo.ts
+++ projects/rectangle-ui/src/lib/components/skeleton/skeleton.component.demo.ts
@@ -1,15 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, HostBinding } from "@angular/core";
 import { SkeletonComponent } from "./skeleton.component";
 
 @Component({
   selector: "rui-skeleton-demo",
   template: `
-    <div class="w-full max-w-md space-y-2">
-      <rui-skeleton height="1.25rem"></rui-skeleton>
-      <rui-skeleton width="80%" height="1.25rem"></rui-skeleton>
-      <rui-skeleton width="60%" height="1.25rem"></rui-skeleton>
+    <div class="w-full max-w-md rounded-2xl border border-primary-300 bg-primary-50 p-4 dark:border-primary-800 dark:bg-primary-900/40">
+      <div class="flex items-center gap-3">
+        <rui-skeleton width="3rem" height="3rem"></rui-skeleton>
+        <div class="flex-1 space-y-2">
+          <rui-skeleton width="45%" height="1rem"></rui-skeleton>
+          <rui-skeleton width="70%" height="0.875rem"></rui-skeleton>
+        </div>
+      </div>
+
+      <div class="mt-4 space-y-2">
+        <rui-skeleton height="1rem"></rui-skeleton>
+        <rui-skeleton width="92%" height="1rem"></rui-skeleton>
+        <rui-skeleton width="78%" height="1rem"></rui-skeleton>
+      </div>
     </div>
   `,
   imports: [SkeletonComponent],
 })
-export class SkeletonDemoComponent {}
+export class SkeletonDemoComponent {
+  @HostBinding("class") hostClasses = "flex w-full justify-center";
+}
