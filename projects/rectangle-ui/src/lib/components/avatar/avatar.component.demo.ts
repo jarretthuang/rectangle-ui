535143d790aefa9fd50c4a65c5475ae529e2d6ae
Jarrett Huang <jarrett@jhuang.ca>
2026-04-18 00:11:43 -0700

    Replace the avatar demo image asset.

--- projects/rectangle-ui/src/lib/components/avatar/avatar.component.demo.ts
+++ projects/rectangle-ui/src/lib/components/avatar/avatar.component.demo.ts
@@ -5,7 +5,7 @@ import { AvatarComponent } from "./avatar.component";
   template: `
     <div class="flex flex-wrap items-center gap-4">
       <div class="flex flex-col items-center gap-2 text-xs text-primary-700 dark:text-primary-200">
-        <rui-avatar src="/img/sad_pikachu.png" alt="Pikachu avatar" fallback="PK"></rui-avatar>
+        <rui-avatar src="/img/pikachu_avatar.jpg" alt="Pikachu avatar" fallback="PK"></rui-avatar>
         <span>Image</span>
       </div>
 
